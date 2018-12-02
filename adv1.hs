ac59235b602256922ce986e50a091c832b505bd2
nek28 <vladinekolov@gmail.com>
2018-12-02 13:53:14 +0200

    restructure fRInf to use cycle

--- adv1.hs
+++ adv1.hs
@@ -1,27 +1,23 @@
 module Main where
 
-import Data.List
 import qualified Data.IntSet as S
 
+--gets the input data string (lines of the form "[+-]NUM") and turns it into [Int]
 asDigits :: String -> [Int]
 asDigits = (fmap (read :: String -> Int)) . words . filterPlus
     where filterPlus = filter (/= '+')
 
-firstRepeat' :: [Int] -> Int
-firstRepeat' ls = firstRepeat' ls 0 (S.fromList [0])
-    where firstRepeat' [] acc partial = firstRepeat' ls acc partial
-          firstRepeat' (x:xs) acc partial = let newV = x + acc in
-                                            if newV `S.member` partial
-                                            then newV
-                                            else firstRepeat' xs newV (S.insert newV partial)
-
-wrangle :: String -> Int
-wrangle = sumit . asDigits
-    where sumit = foldl' (+) 0
+--solves the "first repeating frequency" problem on input
+fRInf :: [Int] -> Int
+fRInf ls = fRInf' (cycle ls) 0 (S.fromList [0])
+    where fRInf' (x:xs) acc oldFreqs = let newFreq = acc + x
+                                       in if newFreq `S.member` oldFreqs
+                                          then newFreq
+                                          else fRInf' xs newFreq (S.insert newFreq oldFreqs)
 
 main :: IO()
 main = do
     str <- readFile "data/1"
     let wrangled = asDigits str
     putStrLn $ "The final frequency is " ++ show (sum wrangled)
-    putStrLn $ "The first repeating frequency is " ++ show (firstRepeat' wrangled)
+    putStrLn $ "The first repeating frequency is " ++ show (fRInf wrangled)
