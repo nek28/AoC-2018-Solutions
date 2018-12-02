63148d9579958a03be8cd4f0911d73b7c442d3b6
nek28 <vladinekolov@gmail.com>
2018-12-02 12:59:42 +0200

    delete slow firstRepeat

--- adv1.hs
+++ adv1.hs
@@ -7,12 +7,6 @@ asDigits :: String -> [Int]
 asDigits = (fmap (read :: String -> Int)) . words . filterPlus
     where filterPlus = filter (/= '+')
 
-firstRepeat :: ([Int],[Int]) -> Int -> Int
-firstRepeat (st,(x:xs)) acc = if not (newV `elem` st)
-                              then firstRepeat (newV:st,xs) newV
-                              else newV
-                                where newV = acc + x
-
 firstRepeat' :: [Int] -> Int
 firstRepeat' ls = firstRepeat' ls 0 (S.fromList [0])
     where firstRepeat' [] acc partial = firstRepeat' ls acc partial
