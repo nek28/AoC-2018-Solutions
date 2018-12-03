64807e3efe68cbe7b09cb125da4fcee34a3b40a5
nek28 <vladinekolov@gmail.com>
2018-12-03 13:42:58 +0200

    write comments for the functions

--- 2/solution.hs
+++ 2/solution.hs
@@ -2,12 +2,20 @@ module Main where
 
 import Data.List
 
+--transforms a string into a "histogram" of how many times each letter is repeated
+--in alphabetic order
 transf :: String -> [Int]
 transf str = fmap length (group . sort $ str)
 
+--checks if any letter is repeated a certain number, which is given as a predicate
+--the function itself is more general, accepting any (Int->Bool) predicate
+--but is used for that purpose
 checkfor :: (Int -> Bool) -> [Int] -> Bool
 checkfor p = or . (fmap p)
 
+--In the puzzle, we are asked to compute a simple checksum on the list of input strings
+--(2/data), namely the product of the number of strings that have a letter repeating 
+--exactly two times and those that have one repeating three times
 checksum :: [String] -> Int
 checksum strs = twos * threes
     where twos = length $ stringsWithNs 2
