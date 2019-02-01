116d78c3697d2c109e4b6ac41c602d720ab94ef3
Vladi Nekolov <vladinekolov@gmail.com>
2019-02-02 00:11:41 +0200

    Update solution.hs

--- 1/solution.hs
+++ 1/solution.hs
@@ -19,7 +19,7 @@ fRInf ls = fRInf' (cycle ls) 0 (Set.fromList [0])
 
 main :: IO()
 main = do
-    inp <- readFile "1/data"
+    inp <- readFile "data"
     let wrangled = asDigits inp
     putStrLn $ "The final frequency is " ++ show (sum wrangled)
     putStrLn $ "The first repeating frequency is " ++ show (fRInf wrangled)
